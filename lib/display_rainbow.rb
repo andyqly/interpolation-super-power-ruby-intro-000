f65d2693ae60379ccfc817fbc7ea2d49a244995e
Andy L <andyqly@hotmail.com>
2019-10-21 13:08:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,11 @@
 def display_rainbow(color)
-  R = red
-  O = orange
-  Y = yellow
-  G = green
-  B = blue
-  I = indigo
-  V = violet
+  "R" = "red"
+  "O" = "orange"
+  "Y" = "yellow"
+  "G" = "green"
+  "B" = "blue"
+  "I" = "indigo"
+  "V" = "violet"
 
   puts "#{display_rainbow}()"
 
